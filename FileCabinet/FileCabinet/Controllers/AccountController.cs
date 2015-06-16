69a44e7d9f45a1c50b764703715c82ebc43e6841
as003611 <amag2511@gmail.com>
2015-06-16 14:01:57 +0300

    mod

--- FileCabinet/FileCabinet/Controllers/AccountController.cs
+++ FileCabinet/FileCabinet/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -87,10 +88,7 @@ namespace FileCabinet.Controllers
         public ActionResult Profile(string category)
         {
             var user = Repository.GetAllUsers.FirstOrDefault(x => x.Username == User.Identity.Name);
-            if (category == "Info")
-                ViewBag.Categ = category;
-            else if (category == "Articles")
-                ViewBag.Categ = category;
+            ViewBag.Categ = category;
             return View(user);
         }
 
@@ -103,14 +101,23 @@ namespace FileCabinet.Controllers
         }
 
         [ChildActionOnly]
-        public PartialViewResult PartOfProfile(UserProfile user, string category)
+        public ActionResult PartOfProfile(UserProfile user, string category)
         {
             if (category == "Info")
                 return PartialView("_PersonalInfo", user);
-            IEnumerable<Article> articles = Repository.GetAllArticles;
-            if(!Roles.GetRolesForUser(User.Identity.Name).Contains("Admin"))
-                articles = articles.Where(x => x.UserProfileId == WebSecurity.CurrentUserId);
-            return PartialView("_PersonalArticles", articles);
+            if (category == "Articles")
+            {
+                IEnumerable<Article> articles = Repository.GetAllArticles;
+                if (!Roles.GetRolesForUser(User.Identity.Name).Contains("Admin"))
+                    articles = articles.Where(x => x.UserProfileId == WebSecurity.CurrentUserId);
+                return PartialView("_PersonalArticles", articles);
+            }
+            else if (category == "Users" && Roles.GetRolesForUser(User.Identity.Name).Contains("Admin"))
+            {
+                var users = Repository.GetAllUsers.SkipWhile(x => x.Username == User.Identity.Name);
+                return PartialView("_Users", users);
+            }
+            return View("Error");
         }
 
         [Authorize]
@@ -124,6 +131,47 @@ namespace FileCabinet.Controllers
                 return RedirectToAction("Profile");
             }
             return View(user);
+        }
+
+        [Authorize(Roles="Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserProfile user = Repository.FindUserById((int)id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            if (user.UserProfileId == WebSecurity.CurrentUserId)
+                return RedirectToAction("Profile");
+            return View(user);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var user = Repository.GetAllUsers.FirstOrDefault(x => x.UserProfileId == id);
+            try
+            {
+                // TODO: Add delete logic here
+                if (Roles.GetRolesForUser(user.Username).Count() > 0)
+                {
+                    Roles.RemoveUserFromRoles(user.Username, Roles.GetRolesForUser(user.Username));
+                }
+                ((SimpleMembershipProvider)Membership.Provider).DeleteAccount(user.Username); // deletes record from webpages_Membership table
+                ((SimpleMembershipProvider)Membership.Provider).DeleteUser(user.Username, true); // deletes record from UserProfile table
+
+                return RedirectToAction("Profile", new { category = "Users" });
+            }
+            catch
+            {
+                return View(id);
+            }
         }
 
         protected override void Dispose(bool disposing)
